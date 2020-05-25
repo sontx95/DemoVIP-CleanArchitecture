220aea1cecab3d84b6303f536f34a00ecf24bc5e
SonTX <sontx@vnpay.vn>
2020-05-25 11:59:53 +0700

    Initial Commit

---
+++ DemoVIPSonTX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoVIPSonTX
+//
+//  Created by iOS Team on 5/25/20.
+//  Copyright © 2020 iOS Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
